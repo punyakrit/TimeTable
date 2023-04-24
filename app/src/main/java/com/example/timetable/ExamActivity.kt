af821d96d4bd20e94f319b94b64c72e8f9abe0c9
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2023-04-24 23:26:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/timetable/ExamActivity.kt
@@ -0,0 +1,27 @@
+package com.example.timetable
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+
+class ExamActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exam)
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+}
